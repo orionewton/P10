206f31f974feec090f24d5bce0e24a8b52035f54
Loan Tytgat <loantytgat@gmail.com>
2020-04-13 11:11:50 +0200

    test db changes

--- PB_django/settings.py
+++ PB_django/settings.py
@@ -86,15 +86,15 @@ WSGI_APPLICATION = 'PB_django.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#   'default': {
+#       'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
-DATABASES['default']['NAME'] = 'alimentdb'
+DATABASES['default']['NAME'] = 'DB'
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
