b0174121ca314b94fac5ace59b1eaa38f802ce72
Loan Tytgat <loantytgat@gmail.com>
2020-04-20 17:47:00 +0200

    statifiles

--- PB_django/settings.py
+++ PB_django/settings.py
@@ -131,7 +131,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+if DEBUG == False:
+    STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+    
 STATIC_URL = '/static/'
 
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
