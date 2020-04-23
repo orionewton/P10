08ae596b0e8f0c6b57ef27c794cb771dbdf93625
Loan Tytgat <loantytgat@gmail.com>
2020-04-23 15:01:52 +0200

    corr statics

--- PB_django/settings.py
+++ PB_django/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, '/static'),
+    os.path.join(BASE_DIR, 'catalog/static'),
 )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
