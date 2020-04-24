13f93f53d3aa5ef8ba14411afdeb7290a0c55971
Loan Tytgat <loantytgat@gmail.com>
2020-04-24 15:40:50 +0200

    wsgi updt

--- PB_django/settings.py
+++ PB_django/settings.py
@@ -136,6 +136,8 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'catalog/static'),
 )
 
+STATICFILES_STORAGE = whitenoise.storage.CompressedManifestStaticFilesStorage
+
 LOGIN_REDIRECT_URL = 'catalog/index'
 LOGIN_URL = 'login'
 
