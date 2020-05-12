41d4302fb416e70eaff062efe988257639f32bb8
Loan Tytgat <loantytgat@gmail.com>
2020-05-12 18:11:34 +0200

    corr

--- PB_django/settings/__init__.py
+++ PB_django/settings/__init__.py
@@ -21,7 +21,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^u+ji#i3ty79@ncf=--)o^(*ip+!oltt@!xxy2j_mdob9z-&n@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
